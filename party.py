f8b268309cec35b60966ab182af011467cca55b9
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-18 12:45:45 -0700

    Write doctests for 'is_mel' and 'most_and_least_common_type'

--- party.py
+++ party.py
@@ -8,7 +8,23 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+    
+    Examples:
+        >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+        True
+
+        >>> is_mel('Judith Butler', 'judith@awesome.com')
+        False
+
+        >>> is_mel('Mel Melitpolski', 'anjelica@ubermelon.com')
+        True
+
+        >>> is_mel('Masha Melitpolski', 'mel@ubermelon.com')
+        True
+
+
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
@@ -17,6 +33,14 @@ def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+    
+    Example:
+
+        >>> treats = [{'type': 'dessert'},{'type': 'dessert'},{'type': 'appetizer'},{'type': 'dessert'},{'type': 'appetizer'},{'type': 'drink'}]
+        
+        >>> most_and_least_common_type(treats)
+        ('dessert', 'drink')
+
     """
 
     types = {}
@@ -39,6 +63,9 @@ def most_and_least_common_type(treats):
             least_type = treat_type
 
     return (most_type, least_type)
+
+
+
 
 
 def get_treats():
